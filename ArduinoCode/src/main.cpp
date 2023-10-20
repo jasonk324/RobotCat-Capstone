571e098625188a03113df5aa484d88ef1ce2cb01
Jason <jasonkhuu.01@gmail.com>
2023-10-19 23:02:25 -0400

    Milestone 2 Arduino

--- ArduinoCode/src/main.cpp
+++ ArduinoCode/src/main.cpp
@@ -1,23 +1,53 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-
+Servo servo1;
+Servo servo2;
 int pos = 0;    // variable to store the servo position
+char piInput;
+
+void servoSpin() {
+
+  switch (piInput) {
+    case '1':
+      Serial.println("SERVO 1 IS ON");
+      for (pos = 0; pos <= 180; pos += 1) { 
+        servo1.write(pos);            
+        delay(20);                    
+      }
+      for (pos = 180; pos >= 0; pos -= 1) { 
+        servo1.write(pos);         
+        delay(20);                     
+      }
+      break;
+    case '2':
+      Serial.println("SERVO 2 IS ON");
+      for (pos = 0; pos <= 180; pos += 1) { 
+        servo2.write(pos);            
+        delay(20);                    
+      }
+      for (pos = 180; pos >= 0; pos -= 1) { 
+        servo2.write(pos);         
+        delay(20);                     
+      }
+      break;
+    default:
+      Serial.println("Nothing happened");
+      break;
+    }
+}
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);
+  servo1.attach(9); 
+  servo2.attach(11);
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  if (Serial.available() > 0) {
+
+    piInput = Serial.read();
+
+    servoSpin();
   }
 }
