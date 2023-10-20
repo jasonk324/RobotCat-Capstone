8343607eb953977450790e6ed4816a52a3cdd4b4
jasonk324 <jasonkhuu.01@gmail.com>
2023-10-19 23:02:04 -0400

    Milestone 2

--- RaspberryPiCode/main.py
+++ RaspberryPiCode/main.py
@@ -1,1 +1,15 @@
-print("Hello Cat")
+import serial 
+
+ser = serial.Serial("/dev/ttyACM0", 9600)
+
+while True:
+    usrInput = input("Enter '1' or '2' to set a Servo ON: ")
+
+    if usrInput == '1' or usrInput == '2':
+        ser.write(usrInput.encode('utf-8'))
+
+        response = ser.readline().decode('utf-8')
+
+        print(response)
+    else:
+        print("Invalid input")
