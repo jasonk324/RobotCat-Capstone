e55a00bda4d28c4075deb5926cef2865576ee0f9
Jason <jasonkhuu.01@gmail.com>
2024-01-22 03:20:05 -0500

    update

--- RobotController/frontend/src/App.jsx
+++ RobotController/frontend/src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Video from './pages/Video';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <div className="main font-NotoSans">
+    	<Header />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
--- RobotController/frontend/src/components/ToPath.jsx
+++ RobotController/frontend/src/components/ToPath.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import CameraIcon from "../assets/Buttons/Camera.png"
+import { Link } from "react-router-dom"
 
-const ToCamera = () => {
+const ToPath = ({Path, Icon, Name}) => {
   return (
-    <div>
+    <Link to={Path}>
         <button 
             className="bg-[#EA89D7] hover:bg-[#a35f96] text-norm text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline bg-shadow flex flex-row items-center gap-2"
         >
-            <img src={CameraIcon} className='w-6'/>
-            Camera View
+            <img src={Icon} className='w-6'/>
+            {Name}
         </button>
-    </div>
+    </Link>
   )
 }
 
-export default ToCamera
+export default ToPath
