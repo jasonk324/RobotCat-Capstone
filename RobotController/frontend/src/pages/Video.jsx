4df36c54ee513f58c53928db5ea8e7e4c3b8224a
Jason <jasonkhuu.01@gmail.com>
2024-01-22 05:21:37 -0500

    video updates

--- RobotController/frontend/src/pages/Video.jsx
+++ RobotController/frontend/src/pages/Video.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+import ToPath from '../components/ToPath';
+import Control from "../assets/Buttons/Control.png"
 
 const Video = () => {
+  const [imageUrl, setImageUrl] = useState(null);
+
+  useEffect(() => {
+    const UpdateImage = async () => {
+      const storage = getStorage();
+      const storageRef = ref(storage, 'streaming/latestImage.jpg');
+  
+      getDownloadURL(storageRef)
+        .then((url) => {
+          setImageUrl(url);
+        })
+        .catch((error) => {
+          console.error('Error getting download URL:', error);
+        });
+    }
+
+    UpdateImage()
+
+    const intervalId = setInterval(UpdateImage, 100);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <>
-      <div>
-        
+      <div className='mt-24 flex flex-col justify-center items-center'>
+        {imageUrl ? (
+          <img src={imageUrl} className='max-w-[90%]'/>
+        ) : (
+          <p>Loading...</p>
+        )}
+				<div className='flex justify-end my-3'>
+          <ToPath Path={"/"} Icon={Control} Name={"Dashboard"}/>
+				</div>
       </div>
     </>
   )
